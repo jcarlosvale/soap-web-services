4513f5be559c5adb8d1682d0a7c522f3dbb2ac89
Joao Carlos <jcarlos.vale@gmail.com>
2018-04-03 23:15:33 -0300

    XSD created and JAXB plugin to generate classes.

---
+++ src/main/java/com/in28minutes/courses/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
+// Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
+// Gerado em: 2018.04.03 às 11:14:21 PM GFT 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://in28minutes.com/courses", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.in28minutes.courses;
